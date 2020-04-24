21b48cc79e31b9c4d0685dc53c2df19651c449af
italo041 <38597283+italo041@users.noreply.github.com>
2020-04-24 02:10:06 -0500

    Pequeño cambio

--- src/Components/CardQuestionNumber/CardQuestionNumber.js
+++ src/Components/CardQuestionNumber/CardQuestionNumber.js
@@ -7,7 +7,8 @@ const CardQuestionNumber = (props) => {
  
             <Card className="card-question-number">
                 <Card.Body className="card-question-number-body">
-                   <div className="font-weight-bold">Pregunta {step}</div>  
+                   <div className=" text-secondary font-weight-bold">Pregunta {step}</div>  
+                   <div className=" text-secondary">Puntaje: 4</div>  
                 </Card.Body>
             </Card>
 
